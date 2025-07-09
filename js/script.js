74d08fdb2d856d9c38b18d47ba0b522958026d51
ray <dimakulangan11+github@gmail.com>
2025-07-09 23:04:35 +0800

    click button event

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const title = document.querySelector('h1')
+const btn = document.querySelector('button')
+
 title.addEventListener('click', function(){
     this.style.color = 'blue'
     alert('you just change title color! Great Job!')
+})
+
+btn.addEventListener('click', function(){
+    this.style.width = '100%'
 })
